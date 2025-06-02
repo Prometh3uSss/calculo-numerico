d11d8a80c5ed8eeea89c763f20cf62a2fc709770
Prometh3uSss <romardavid.4126@gmail.com>
2025-06-01 20:44:31 -0400

    Co-authored-by: Lulujax <Lulujax@users.noreply.github.com>

---
+++ SistemaNumerico.py
@@ -0,0 +1,20 @@
+
+
+#Como lo clonaba?
+"voy"
+"git clone https://github.com/Prometh3uSss/calculo-numerico.git"
+"tienes que abrir la terminal git bash dentro de la carpeta que creaste."
+"una vez abierto el terminal, copia lo que puse"
+"ahora como subo todo XDD"
+"esta es la parte heavy"
+"porque tienes que hacer el flujo de trabajo que explico e profesor"
+"aqui deepseek me lo explico ya te lo paso"
+""
+
+"Me sale solo la rama master, no me sale las demas" 
+"en donde?"
+"mrk puedes hacer un meet asi se me hace tedioso entender algunas cosas"
+
+"Discord es mas facil agrega, Lulujax"
+"Tambien tengo discord, dale"
+"Voy"
