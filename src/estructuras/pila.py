b4989033ae9613c2ca55195c699e8e3153657063
Samulon10 <samuelrojas.oliveros.2003@gmail.com>
2025-06-10 01:12:12 -0400

    Agregando funcionalidad pila.py

---
+++ src/estructuras/pila.py
@@ -0,0 +1,30 @@
+# Proyecto_Calculo_Numerico/src/estructuras/pila.py
+from .lista_enlazada import ListaEnlazada
+
+class Pila:
+    def _init_(self):
+        self.datos = ListaEnlazada()
+    
+    def apilar(self, elemento):
+        self.datos.agregar(elemento)
+    
+    def desapilar(self):
+        if self.esta_vacia():
+            raise IndexError("Pila vacía")
+        
+        if len(self.datos) == 1:
+            dato = self.datos.obtener(0)
+            self.datos = ListaEnlazada()
+            return dato
+        
+        actual = self.datos.cabeza
+        for _ in range(len(self.datos) - 2):
+            actual = actual.siguiente
+        dato = actual.siguiente.dato
+        actual.siguiente = None
+        self.datos.longitud -= 1
+        return dato
+    
+    def esta_vacia(self):
+        return len(self.datos) == 0
+    
