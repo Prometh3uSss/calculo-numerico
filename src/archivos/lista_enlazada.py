b645819f03cc299fb58f8f64a7b14ecdce6f5b6c
Prometh3uSss <romardavid.4126@gmail.com>
2025-06-10 00:39:12 -0400

    Actualizacion archivo lista_enlazada.py

--- src/archivos/lista_enlazada.py
+++ src/archivos/lista_enlazada.py
@@ -8,3 +8,31 @@ class ListaEnlazada:
     def __init__(self):
         self.cabeza = None
         self.longitud = 0
+
+    def agregar(self, dato):
+        nuevo_nodo = Nodo(dato)
+        if not self.cabeza:
+            self.cabeza = nuevo_nodo
+        else:
+            actual = self.cabeza
+            while actual.siguiente:
+                actual = actual.siguiente
+            actual.siguiente = nuevo_nodo
+        self.longitud += 1
+
+    def obtener(self, indice):
+        if indice < 0 or indice >= self.longitud:
+            raise IndexError("Índice fuera de rango")
+        actual = self.cabeza
+        for _ in range(indice):
+            actual = actual.siguiente
+        return actual.dato
+
+    def __len__(self):
+        return self.longitud
+
+    def __iter__(self):
+        actual = self.cabeza
+        while actual:
+            yield actual.dato
+            actual = actual.siguiente
