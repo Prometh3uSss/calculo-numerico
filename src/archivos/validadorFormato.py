9e1592e2380de54f2cf3f4b2f08ec4591c36d283
Samulon10 <samuelrojas.oliveros.2003@gmail.com>
2025-06-20 00:45:18 -0400

    Agregando funcionalidad validadorFormato

---
+++ src/archivos/validadorFormato.py
@@ -0,0 +1,18 @@
+import re
+from datetime import datetime
+
+class ValidadorFormato:
+    @staticmethod
+    def validarNombreArchivo(nombre):
+        patron = r"^(.+)(\d{8})(\d+)\.txt$"
+        coincidencia = re.match(patron, nombre)
+        
+        if not coincidencia:
+            return False
+        
+        try:
+            fechaStr = coincidencia.group(2)
+            datetime.strptime(fechaStr, "%Y%m%d")
+            return True
+        except ValueError:
+            return False
