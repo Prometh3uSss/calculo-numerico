b2169b666846a1f1663cd49addf3f8529619c2d1
Prometh3uSss <romardavid.4126@gmail.com>
2025-06-21 20:36:07 -0400

    Optimizacion codigo cola.py

--- src/estructuras/cola.py
+++ src/estructuras/cola.py
@@ -1,25 +1,32 @@
 from listaEnlazada import ListaEnlazada
 
 class Cola:
-    def _init_(self):
+    def __init__(self):  # Corregido: __init__
         self.datos = ListaEnlazada()
+        self.ultimo = None
     
     def encolar(self, elemento):
-        self.datos.agregar(elemento)
+        nuevo_nodo = Nodo(elemento)
+        if not self.datos.cabeza:
+            self.datos.cabeza = nuevo_nodo
+            self.ultimo = nuevo_nodo
+        else:
+            self.ultimo.siguiente = nuevo_nodo
+            self.ultimo = nuevo_nodo
+        self.datos.longitud += 1
     
     def desencolar(self):
-        if self.estaVacia():
-            raise IndexError("Cola vacia")
-        
+        if self.esta_vacia():
+            raise IndexError("Cola vacía")
         dato = self.datos.cabeza.dato
         self.datos.cabeza = self.datos.cabeza.siguiente
         self.datos.longitud -= 1
+        if self.esta_vacia():
+            self.ultimo = None
         return dato
     
-    def estaVacia(self):
-        return len(self.datos) == 0
+    def esta_vacia(self):
+        return self.datos.longitud == 0
     
     def frente(self):
-        if self.estaVacia():
-            return None
-        return self.datos.obtener(0)
+        return self.datos.cabeza.dato if not self.esta_vacia() else None
